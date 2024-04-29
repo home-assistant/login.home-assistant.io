8aa6356d670227f14006b4325e74ca867bf7f5c5
Bram Kragten <mail@bramkragten.nl>
2024-04-29 15:02:55 +0200

    Update layout

--- src/entrypoints/login-index.ts
+++ src/entrypoints/login-index.ts
@@ -1,4 +1,4 @@
-import { LitElement, TemplateResult, html } from "lit";
+import { LitElement, PropertyValues, css, html, nothing } from "lit";
 import { customElement, state } from "lit/decorators.js";
 import { extractSearchParam } from "../util/search-params";
 import { validUrl } from "../util/validate";
@@ -8,10 +8,13 @@ class LoginIndex extends LitElement {
   @state() private _error?: string;
 
   createRenderRoot() {
-    while (this.lastChild) {
-      this.removeChild(this.lastChild);
-    }
     return this;
+  }
+
+  protected updated(changedProperties: PropertyValues) {
+    if (changedProperties.has("_error") && this._error) {
+      this.renderRoot.querySelector("#loading")?.remove();
+    }
   }
 
   public connectedCallback() {
@@ -33,11 +36,16 @@ class LoginIndex extends LitElement {
     window.location.replace(url);
   }
 
-  protected render(): TemplateResult {
+  protected render() {
     if (this._error) {
-      return html`<div class="error">${this._error}</div>`;
+      return html`<div class="error" role="alert">
+        <div class="error-icon">
+        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><path d="M13,13H11V7H13M13,17H11V15H13M12,2A10,10 0 0,0 2,12A10,10 0 0,0 12,22A10,10 0 0,0 22,12A10,10 0 0,0 12,2Z" /></svg>
+        </div>
+        <div class="error-content">${this._error}</div>
+      </div>`;
     }
-    return html` Loading... `;
+    return nothing;
   }
 }
 
